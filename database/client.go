4465290cb1351f71d7222cf1e6a243a2dcc1d529
akalankaih <akalankaih.19@uom.lk>
2022-05-03 10:02:58 +0530

    first commit

---
+++ database/client.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"main.go/models"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "root:iha075@tcp(127.0.0.1:3306)/projectTest?charset=utf8mb4&parseTime=True&loc=Local"
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+}
+
