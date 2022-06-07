71de0dfe7312ae17405923239519f3238c0fc2aa
akalankaih <akalankaih.19@uom.lk>
2022-06-08 03:45:08 +0530

    Add verify token middleware to the authRoutes file

--- routes/authRoutes.go
+++ routes/authRoutes.go
@@ -8,6 +8,9 @@ import (
 func Route(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+
+	app.Use(controllers.VerifyToken)
+
 	app.Post("/api/logout", controllers.Logout)
 	app.Post("/api/forgot", controllers.Forgot)
 	app.Post("/api/reset", controllers.Reset)
