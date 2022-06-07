f2ff71a29d856c5d110ca5de470788f94cdd7bad
akalankaih <akalankaih.19@uom.lk>
2022-06-08 03:54:55 +0530

    Add port to the .env file

--- main.go
+++ main.go
@@ -3,23 +3,30 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 	"log"
 	"main.go/database"
 	"main.go/routes"
+	"os"
 )
 
 func main() {
 	database.Connect()
+
+	godotenv.Load()
+
 	app := fiber.New()
+
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 		AllowOrigins:     "*",
-		
 	}))
+
 	routes.Route(app)
 	routes.Categoryroute(app)
 	routes.Productroute(app)
 	routes.Roleroute(app)
 
-	log.Fatal(app.Listen(":8080"))
+	port := os.Getenv("PORT")
+	log.Fatal(app.Listen(port))
 }
